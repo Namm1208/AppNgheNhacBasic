66e0e0c3e8a69fec625a6f7ac8096171c85b5532
Namm1208 <125287992+Namm1208@users.noreply.github.com>
2025-04-10 21:48:03 +0700

    final music app

---
+++ app/src/main/java/com/example/music/constant/AboutUsConfig.java
@@ -0,0 +1,17 @@
+package com.example.music.constant;
+
+public interface AboutUsConfig {
+    String ABOUT_US_TITLE = "Pham Hai Nam";
+    String ABOUT_US_CONTENT = "Basic music app java.";
+    String ABOUT_US_SLOGAN = "Highly products & excellent sound";
+    String ABOUT_US_WEBSITE_TITLE = "2121051532";
+
+    String PAGE_FACEBOOK = "fb://page/PhamHaiNam";
+    String LINK_FACEBOOK = "https://www.facebook.com/NamHaiPham";
+    String LINK_YOUTUBE = "https://www.youtube.com/";
+    String PHONE_NUMBER = "+84336657990";
+    String GMAIL = "namp6348@gmail.com";
+    String SKYPE_ID = "Namm1208";
+    String ZALO_LINK = "https://zalo.me/0336657990";
+    String WEBSITE = "https:";
+}
